2cb6c1e8cd1e797d5a38a573c3bf76dbd18bf9b3
thasup <thanachonfirst@hotmail.com>
2024-05-08 22:03:16 +0700

    task: Prepare card dto

--- src/modules/card/card.module.ts
+++ src/modules/card/card.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Card } from './entities/card.entity';
 import { CardRepository } from './repositories/customer.repository';
+import { CardService } from './services/card.service';
+import { CardController } from './controllers/card.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Card])],
-  providers: [CardRepository],
+  providers: [CardRepository, CardService],
   exports: [],
+  controllers: [CardController],
 })
 export class CardModule {}

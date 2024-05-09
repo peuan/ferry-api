2bdebd3817c8f752a03880c0d4334fe9d4d5025c
rmuti56 <chok.watcharathep@maqe.com>
2024-05-09 09:46:52 +0700

    task(media): add service and controller

--- src/modules/media/media.module.ts
+++ src/modules/media/media.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Media } from './entities/media.entity';
 import { MediaRepository } from './repositories/media.repository';
+import { MediaService } from './services/media.service';
+import { MediaController } from './controllers/media.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Media])],
-  providers: [MediaRepository],
+  providers: [MediaRepository, MediaService],
+  controllers: [MediaController],
 })
 export class MediaModule {}
